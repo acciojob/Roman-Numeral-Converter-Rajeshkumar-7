58094ea729e79f9cf11d15f31e262c29550b5977
Rajeshkumar-7 <83906427+Rajeshkumar-7@users.noreply.github.com>
2024-03-26 17:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,96 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+   // romanNumerals is an array of special Roman numerals. numbers is an array of their equivalent Arabic numerals
+    let numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    let romanNumerals = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+    let romNum = ""; // empty string to hold Roman numerals not in romanNumerals array
 
-  //your code here
+    // When num is in numbers array above
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === num) {
+            return romanNumerals[i];
+        }
+    }
+    // When num is less than 4 but not in numbers array
+    if (num < 4) {
+        for (let i = 1; i <= num; i++) {
+            romNum = romNum + 'I';
+        }
+    }
+    // When num is between 4 and 9 but not in numbers array 
+    else if (num < 9) {
+        romNum = 'V';
+        for (let i = 6; i <= num; i++) {
+            romNum = romNum + 'I';
+        }
+    }
+    // When num is between 9 and 40 but not in numbers array 
+    else if (num < 40) {
+        let tensPart = Math.floor(num / 10);
+        let remainder = num % 10;
+        for (let i = 0; i < tensPart; i++) {
+            romNum = romNum + 'X';
+        }
+        romNum = romNum + convertToRoman(remainder); 
+    }
+    // When num is between 40 and 50 but not in numbers array
+    else if (num < 50) {
+        let remainder = num % 10;
+        romNum = 'XL' + convertToRoman(remainder);
+    }
+    // When num is between 50 and 90 but not in numbers array
+    else if (num < 90) {
+        romNum = 'L';
+        let tensPart = Math.floor(num / 10);
+        let remainder = num % 10;
+        for (let i = 5; i < tensPart; i++) {
+            romNum = romNum + 'X';
+        }
+        romNum = romNum + convertToRoman(remainder);
+    }
+    // When num is between 90 and 100 but not in numbers array
+    else if (num < 100) {
+        let remainder = num % 10;
+        romNum = 'XC' + convertToRoman(remainder);
+    }
+    // When num is between 100 and 400 but not in numbers array
+    else if (num < 400) {
+        let hundredPart = Math.floor(num / 100);
+        let remainder = num % 100;
+        for (let i = 0; i < hundredPart; i++) {
+            romNum = romNum + 'C';
+        }
+        romNum = romNum + convertToRoman(remainder);
+    }
+    // When num is between 400 and 500 but not in numbers array
+    else if (num < 500) {
+        let remainder = num % 100;
+        romNum = 'CD' + convertToRoman(remainder);
+    }
+    // When num is between 500 and 900 but not in numbers array
+    else if (num < 900) {
+        romNum = 'D';
+        let hundredPart = Math.floor(num / 100);
+        let remainder = num % 100;
+        for (let i = 5; i < hundredPart; i++) {
+            romNum = romNum + 'C';
+        }
+        romNum = romNum + convertToRoman(remainder);
+    }
+    // When num is between 900 and 1000 but not in numbers array
+    else if (num < 1000) {
+        let remainder = num % 100;
+        romNum = 'CM' + convertToRoman(remainder);
+    }
+    // When num is between 1000 and 4000 but not in numbers array
+    else {
+        let thousandPart = Math.floor(num / 1000);
+        let remainder = num % 1000;
+        for (let i = 0; i < thousandPart; i++) {
+            romNum = romNum + 'M';
+        }
+        romNum = romNum + convertToRoman(remainder);
+    }
+    return romNum;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
